3335876ec1108a68573c76ef855537ed05024cb8
Maarten Van Genechten <maarten.vangenechten@outlook.com>
2025-05-04 20:33:54 +0200

    fooi_pp toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,10 @@ def decoreer(tekst = ""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        berdrag_pp = bedrag/personen
+    except:
+        berdrag_pp = "??"
+    return f"Het bedrag per persoon is {berdrag_pp} euro"
